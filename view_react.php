c1b39437ab85828fb8dbb674d88937385625095e
Hill <hill@kimo.com>
2016-01-24 18:37:29 +0000

    improve first byte performance

--- view_react.php
+++ view_react.php
@@ -16,20 +16,23 @@ class view_react extends ViewEngine
         if (empty($this->node)) {
             return false;
         }
-        $get = escapeshellarg($this['react_data']);
         // node ../themes/react_case/server.js '{"path":"home"}'
-        return shell_exec($this->node.' '.$this['themeDir'].'/server.js '.$get);
+        $cmd = $this->node.' '.$this['themeDir'].'/server.js '.escapeshellarg($this['react_data']);
+        return shell_exec($cmd);
     }
 
     public function process()
     {
         if (!\PMVC\realpath($this['themeDir'])) {
-            trigger_error('Template folder was not found: ['.$this['themeDir'].']');
-            return;
+            return !trigger_error('Template folder was not found: ['.$this['themeDir'].']');
         }
         $t = $this->initTemplateHelper($this['themeDir']);
-        $file = $this->getTplFile($this['themePath']);
         if (empty($this['run'])) {
+            $headFile = $this->getTplFile('head');
+            if (\PMVC\realpath($headFile)) {
+                include($headFile);
+                flush();
+            }
             $this['react_data'] = json_encode($this->get());
             $run = trim($this->run());
             $separator = '<!--start-->';
@@ -42,6 +45,7 @@ class view_react extends ViewEngine
                 $this['run'] = $run; 
             }
         }
+        $file = $this->getTplFile($this['themePath']);
         if (\PMVC\realpath($file)) {
             include($file);
         } else {
